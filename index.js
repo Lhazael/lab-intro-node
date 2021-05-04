13a5befc89ebc76de2215ecfb5df79f22e5cb907
Lhazael <lisa.hazael@gmail.com>
2021-05-04 10:14:35 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.length = 0; //see index.spec line 12
+    this.items = []; // see index.spec line 13
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    console.log(this.items);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length || pos < 0) {
+      throw new Error ("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error ("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error ("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, val) => acc + val, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) throw new Error ("EmptySortedList");
+      return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
